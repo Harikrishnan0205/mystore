6d4e215c058d9fdc077c9e187cc9daa6e082e01b
hari0205 <hari.krishnan@ghtit.com>
2025-06-03 15:56:25 +0530

    file name changed

--- src/pages/SingleProductDetails.jsx
+++ src/pages/SingleProductDetails.jsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import ProductImage from './ProductImage';
-import AboutProduct from './aboutProduct';
+import AboutProduct from './AboutProduct';
 import ProductInfo from './ProductInfo';
 import ProductitemDetails from './ProductItemDetails';
 
